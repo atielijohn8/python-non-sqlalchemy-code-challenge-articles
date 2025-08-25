b7305f5efe0d42faaf0b307bcb7a6726ad87bffb
atielijohn8 <atielijohn4@gmail.com>
2025-08-25 03:44:39 +0300

    Aticle.title

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,6 +3,17 @@ class Article:
         self.author = author
         self.magazine = magazine
         self.title = title
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if hasattr(self, '_title'):
+            return
+        if isinstance(value, str) and 5<= len(value) <= 50:
+            self._title = value
         
 class Author:
     def __init__(self, name):
